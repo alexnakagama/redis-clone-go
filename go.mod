8ea63dffab529da76d3c446ba6198a90696084d2
alexnakagama <alexnakagama4@gmail.com>
2026-07-22 00:50:47 -0300

    added the go mod and function to create a new server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexnakagama/redis-clone-go
+
+go 1.26.4
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,12 +5,20 @@ import (
 )
 
 type Server struct {
-	Address string
+	address  string
+	listener net.Listener
 }
 
-func NewServer(address string) (*Server, error) {
-	listener, err := net.Listen("tcp", ":6379")
+func NewServer(addr string) (*Server, error) {
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
+
+	server := &Server{
+		address:  addr,
+		listener: listener,
+	}
+
+	return server, nil
 }
