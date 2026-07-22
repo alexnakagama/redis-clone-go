d7fe6bc411ee6f456071f4fc6306b6cb5b07e665
alexnakagama <alexnakagama4@gmail.com>
2026-07-22 18:52:30 -0300

    created funcion to process the command

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -1,5 +1,16 @@
 package commands
 
-func Process(message string) {
+import (
+	"strings"
+)
 
+func Process(message string) (string, error) {
+	message = strings.TrimSpace(message)
+
+	switch message {
+	case "PING":
+		return "PONG", nil
+	default:
+		return "ERROR: unknown command", nil
+	}
 }
