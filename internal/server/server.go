e53ebbadec500350c4a4522c710efa7a00934b7d
alexnakagama <alexnakagama4@gmail.com>
2026-07-22 01:11:35 -0300

    server

--- internal/server/server.go
+++ internal/server/server.go
@@ -22,3 +22,27 @@ func NewServer(addr string) (*Server, error) {
 
 	return server, nil
 }
+
+func (s *Server) Start() error {
+	conn, err := s.listener.Accept()
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+
+	data := buffer[:n]
+
+	return nil
+}
+
+func (s *Server) Close() error {
+
+}
