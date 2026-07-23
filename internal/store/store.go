c0dfc8f650cd2da32958630753c040542e2e02d7
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 00:52:55 -0300

    added delete functioin

--- internal/store/store.go
+++ internal/store/store.go
@@ -6,10 +6,13 @@ type Store struct {
 	data map[string]string
 }
 
-func (s *Store) Set(key, value string) {
-
+func (s *Store) Set(key, value string) error {
+	return nil
 }
 
 func (s *Store) Get(key string) (string, bool) {
+}
 
+func (s *Store) Delete(key string) error {
+	return nil
 }
