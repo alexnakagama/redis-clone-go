27f592abbd62bfa0dc2f7e041824048216ed4927
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 00:58:04 -0300

    delete function and set function

--- internal/store/store.go
+++ internal/store/store.go
@@ -7,12 +7,20 @@ type Store struct {
 }
 
 func (s *Store) Set(key, value string) error {
+	s.data[key] = value
 	return nil
 }
 
 func (s *Store) Get(key string) (string, bool) {
 }
 
-func (s *Store) Delete(key string) error {
-	return nil
+func (s *Store) Delete(key string) bool {
+	_, exists := s.data[key]
+
+	if !exists {
+		return false
+	}
+
+	delete(s.data, key)
+	return true
 }
