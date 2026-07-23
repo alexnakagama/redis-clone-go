87a3c8f3b4b579e49db687f8bf9ce9f4125f3000
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 13:52:17 -0300

    added rwmutex

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,11 @@
 package store
 
+import (
+	"sync"
+)
+
 type Store struct {
+	mu   sync.RWMutex
 	data map[string]string
 }
 
@@ -11,11 +16,17 @@ func NewStore() *Store {
 }
 
 func (s *Store) Set(key, value string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.data[key] = value
 	return nil
 }
 
 func (s *Store) Get(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	value, exists := s.data[key]
 
 	if !exists {
@@ -26,6 +37,9 @@ func (s *Store) Get(key string) (string, bool) {
 }
 
 func (s *Store) Delete(key string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	_, exists := s.data[key]
 
 	if !exists {
