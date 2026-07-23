3773c082197366c294d3442654656729c8d0a446
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 01:03:15 -0300

    finsished store file

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,9 +1,13 @@
 package store
 
-import ()
-
 type Store struct {
 	data map[string]string
+}
+
+func NewStore() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
 }
 
 func (s *Store) Set(key, value string) error {
@@ -12,6 +16,13 @@ func (s *Store) Set(key, value string) error {
 }
 
 func (s *Store) Get(key string) (string, bool) {
+	value, exists := s.data[key]
+
+	if !exists {
+		return "", false
+	}
+
+	return value, true
 }
 
 func (s *Store) Delete(key string) bool {
