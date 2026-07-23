07d35de27276a8f88c0899084f593f781ab7be61
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 00:50:25 -0300

    store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,15 @@
 package store
+
+import ()
+
+type Store struct {
+	data map[string]string
+}
+
+func (s *Store) Set(key, value string) {
+
+}
+
+func (s *Store) Get(key string) (string, bool) {
+
+}
