64ec7b644c3b27f2f2996f7924949ecca0e86973
alexnakagama <alexnakagama4@gmail.com>
2026-07-23 01:33:44 -0300

    line jump

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -10,7 +10,7 @@ func Process(message string, s *store.Store) (string, error) {
 	parts := strings.Fields(message)
 
 	if len(parts) == 0 {
-		return "ERROR: empty command", nil
+		return "ERROR: empty command\n", nil
 	}
 
 	command := parts[0]
@@ -22,42 +22,42 @@ func Process(message string, s *store.Store) (string, error) {
 
 	case "GET":
 		if len(parts) < 2 {
-			return "ERROR: missing key", nil
+			return "ERROR: missing key\n", nil
 		}
 
 		value, exists := s.Get(parts[1])
 		if !exists {
-			return "(nil)", nil
+			return "ERROR: value not found\n", nil
 		}
 
 		return value, nil
 
 	case "SET":
 		if len(parts) < 3 {
-			return "ERROR: missing key", nil
+			return "ERROR: missing key\n", nil
 		}
 
 		err := s.Set(parts[1], parts[2])
 
 		if err != nil {
-			return "ERROR: set failed", err
+			return "ERROR: set failed\n", err
 		}
 
-		return "OK", nil
+		return "OK\n", nil
 
 	case "DEL":
 		if len(parts) < 2 {
-			return "ERROR: missing key", nil
+			return "ERROR: missing key\n", nil
 		}
 
 		exists := s.Delete(parts[1])
 		if !exists {
-			return "ERROR: key not found", nil
+			return "ERROR: key not found\n", nil
 		}
 
-		return "OK", nil
+		return "OK\n", nil
 
 	default:
-		return "ERROR: unknown command", nil
+		return "ERROR: unknown command\n", nil
 	}
 }
