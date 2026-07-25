8b27b368ac21be981b589e38fe0bc9c83a62a708
alexnakagama <alexnakagama4@gmail.com>
2026-07-25 15:08:16 -0300

    added the command to the process function

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -102,6 +102,18 @@ func Process(message string, st *store.Store) (string, bool, error) {
 
 		return strings.Join(sKeys, "\n"), false, nil
 
+	case "INCR":
+		if len(parts) < 2 {
+			return "ERROR: missing arguments\n", false, nil
+		}
+
+		intValue, err := st.Incr(parts[1])
+		if err != nil {
+			return "ERROR: " + err.Error() + "\n", false, nil
+		}
+
+		return strconv.Itoa(intValue) + "\n", false, nil
+
 	case "QUIT":
 		return "OK\n", true, nil
 
