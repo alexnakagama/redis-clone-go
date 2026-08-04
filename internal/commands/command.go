2c6d199a239c5253b3b46f0539ce464882bb9b4b
alexnakagama <alexnakagama4@gmail.com>
2026-08-04 15:26:46 -0300

    added decrby function to the process function

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -236,6 +236,23 @@ func Process(message string, st *store.Store) (string, bool, error) {
 
 		return strconv.Itoa(value) + "\n", false, nil
 
+	case "DECRBY":
+		if len(parts) != 3 {
+			return "ERROR: missing arguments\n", false, nil
+		}
+
+		num, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return "ERROR: value is not an integer\n", false, nil
+		}
+
+		value, err := st.DecrBy(parts[1], num)
+		if err != nil {
+			return "ERROR: " + err.Error() + "\n", false, nil
+		}
+
+		return strconv.Itoa(value) + "\n", false, nil
+
 	case "QUIT":
 		return "OK\n", true, nil
 
