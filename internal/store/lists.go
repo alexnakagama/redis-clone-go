23d946dfbfe4181e989d4013de3c4b11e2eb8e66
alexnakagama <alexnakagama4@gmail.com>
2026-08-08 14:33:03 -0300

    finished lpush function

--- internal/store/lists.go
+++ internal/store/lists.go
@@ -1,4 +1,48 @@
 package store
 
-func (s *Store) LPush(key string, value []string) (int, error) {
+import "errors"
+
+func (s *Store) LPush(key string, values []string) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.removeExpired(key)
+
+	value, exists := s.data[key]
+	if !exists {
+		list := make([]string, len(values))
+
+		for i := range values {
+			list[len(values)-1-i] = values[i]
+		}
+
+		s.data[key] = Value{
+			Type: "list",
+			Data: list,
+		}
+
+		return len(list), nil
+	}
+
+	if value.Type != "list" {
+		return 0, errors.New("type mismtach")
+	}
+
+	list, ok := value.Data.([]string)
+	if !ok {
+		return 0, errors.New("type mismtach")
+	}
+
+	newList := make([]string, 0, len(values)+len(list))
+
+	for i := len(values) - 1; i >= 0; i-- {
+		newList = append(newList, values[i])
+	}
+
+	newList = append(newList, list...)
+
+	value.Data = list
+	s.data[key] = value
+
+	return len(newList), nil
 }
