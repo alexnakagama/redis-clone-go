9209c90c90c8002982afb0e948b466dbba22c39d
alexnakagama <alexnakagama4@gmail.com>
2026-08-13 10:17:42 -0300

    finished test for lpush

--- internal/store/lists_test.go
+++ internal/store/lists_test.go
@@ -4,7 +4,48 @@ import (
 	"testing"
 )
 
-func TestLPush(t *testing.T) {}
+func TestLPush(t *testing.T) {
+	store := NewStore()
+
+	names := []string{"alex", "juan"}
+
+	listLen, err := store.RPush("name", names)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if listLen != 2 {
+		t.Fatalf("expected 2, got: %d", listLen)
+	}
+
+	name := []string{"fran"}
+
+	length, err := store.LPush("name", name)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if length != 3 {
+		t.Fatalf("expected 3, got: %d", length)
+	}
+
+	list, err := store.LRange("name", 0, 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if list[0] != "fran" {
+		t.Fatalf("expected fran, got: %s", list[0])
+	}
+
+	if list[1] != "alex" {
+		t.Fatalf("expected alex, got: %s", list[1])
+	}
+
+	if list[2] != "juan" {
+		t.Fatalf("expected juan got: %s", list[2])
+	}
+}
 
 func TestRPush(t *testing.T) {
 	store := NewStore()
