085837906adb52fb29d7d840b1b749ebe6504155
alexnakagama <alexnakagama4@gmail.com>
2026-08-13 10:11:17 -0300

    finsihed test for rpush

--- internal/store/lists_test.go
+++ internal/store/lists_test.go
@@ -6,4 +6,30 @@ import (
 
 func TestLPush(t *testing.T) {}
 
-func TestRPush(t *testing.T) {}
+func TestRPush(t *testing.T) {
+	store := NewStore()
+
+	names := []string{"alex", "juan"}
+
+	listLen, err := store.RPush("name", names)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if listLen != 2 {
+		t.Fatalf("expected 2, got: %d", listLen)
+	}
+
+	list, err := store.LRange("name", 0, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if list[0] != "alex" {
+		t.Fatalf("expected alex, got: %s", list[0])
+	}
+
+	if list[1] != "juan" {
+		t.Fatalf("expected juan, got: %s", list[1])
+	}
+}
