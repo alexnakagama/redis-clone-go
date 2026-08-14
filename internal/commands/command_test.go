792a70def4204234c1f87d4ac6846178e74bb02a
alexnakagama <alexnakagama4@gmail.com>
2026-08-14 17:03:28 -0300

    finished test for process ping

--- internal/commands/command_test.go
+++ internal/commands/command_test.go
@@ -2,6 +2,23 @@ package commands
 
 import (
 	"testing"
+
+	"github.com/alexnakagama/redis-clone-go/internal/store"
 )
 
-func TestProcessPing(t *testing.T) {}
+func TestProcessPing(t *testing.T) {
+	st := store.NewStore()
+
+	response, closeConn, err := Process([]string{"PING"}, st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response != "PONG\n" {
+		t.Errorf("expected %q, got: %q", response, "PONG\n")
+	}
+
+	if closeConn {
+		t.Error("PING should not close the connection")
+	}
+}
