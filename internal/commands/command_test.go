054e147a0c82d60a6c0972f6c48efeefea6f13ea
alexnakagama <alexnakagama4@gmail.com>
2026-08-16 15:41:56 -0300

    finished test for missing key decrby

--- internal/commands/command_test.go
+++ internal/commands/command_test.go
@@ -512,3 +512,20 @@ func TestProcessDecrBy(t *testing.T) {
 		t.Errorf("DECRBY should not close the connection")
 	}
 }
+
+func TestProcessDecrByMissingKey(t *testing.T) {
+	st := store.NewStore()
+
+	response, closeConn, err := Process([]string{"DECRBY"}, st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response != "ERROR: missing arguments\n" {
+		t.Errorf("expected ERROR: missing arguments, got: %q", response)
+	}
+
+	if closeConn {
+		t.Errorf("DECRBY should not close the connection")
+	}
+}
