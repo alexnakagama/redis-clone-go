f2576831c9ec79d3187e96c07de1b98bd0a7cb94
alexnakagama <alexnakagama4@gmail.com>
2026-08-17 01:32:18 -0300

    finished test for missing args touch

--- internal/commands/command_test.go
+++ internal/commands/command_test.go
@@ -1068,4 +1068,19 @@ func TestProcessTouch(t *testing.T) {
 	}
 }
 
-func TestProcessTouchMissingArgs(t *testing.T) {}
+func TestProcessTouchMissingArgs(t *testing.T) {
+	st := store.NewStore()
+
+	response, closeConn, err := Process([]string{"TOUCH"}, st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response != "ERROR: missing arguments\n" {
+		t.Errorf("expected ERROR: missing arguments, got: %q", response)
+	}
+
+	if closeConn {
+		t.Errorf("TOUCH should not close the connection")
+	}
+}
