e564c47221ef47ef3765d5d9204cb62504d8e84c
alexnakagama <alexnakagama4@gmail.com>
2026-08-17 00:58:41 -0300

    finished test for ttl missing args

--- internal/commands/command_test.go
+++ internal/commands/command_test.go
@@ -789,4 +789,19 @@ func TestProcessTTL(t *testing.T) {
 	}
 }
 
-func TestProcessTTLMissingArgs(t *testing.T) {}
+func TestProcessTTLMissingArgs(t *testing.T) {
+	st := store.NewStore()
+
+	response, closeConn, err := Process([]string{"TTL"} ,st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response != "ERROR: missing arguments\n" {
+		t.Errorf("expected ERROR: missing arguments, got: %q", response)
+	}
+
+	if closeConn {
+		t.Errorf("TTL should not close the connection")
+	}
+}
