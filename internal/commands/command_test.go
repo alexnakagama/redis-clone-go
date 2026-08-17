11453af8d49d2ea4109de7009f50aab309f7a099
alexnakagama <alexnakagama4@gmail.com>
2026-08-17 00:37:07 -0300

    finishjed test for mget missing args

--- internal/commands/command_test.go
+++ internal/commands/command_test.go
@@ -667,4 +667,19 @@ func TestProcessMGet(t *testing.T) {
 	}
 }
 
-func TestProcessMGetMissingArgs(t *testing.T) {}
+func TestProcessMGetMissingArgs(t *testing.T) {
+	st := store.NewStore()
+
+	response, closeConn, err := Process([]string{"MGET"}, st)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response != "ERROR: missing arguments\n" {
+		t.Errorf("expected ERROR: missing arguments, got: %q", response)
+	}
+
+	if closeConn {
+		t.Errorf("MGET should not close the connection")
+	}
+}
