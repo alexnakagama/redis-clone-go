3c56072d48e973fcbaf6489cc9d99214f0ff32d2
alexnakagama <alexnakagama4@gmail.com>
2026-08-20 19:43:27 -0300

    finished test for encode a simple str

--- internal/resp/encoder_test.go
+++ internal/resp/encoder_test.go
@@ -1,7 +1,21 @@
 package resp
 
 import (
+	"bytes"
 	"testing"
 )
 
-func TestEncodeSimpleString(t *testing.T) {}
+func TestEncodeSimpleString(t *testing.T) {
+	var buf bytes.Buffer
+
+	err := NewEncoder(&buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "OK\r\n"
+
+	if buf.String() != expected {
+		t.Errorf("expected %q, got: %q", expected, buf.String())
+	}
+}
