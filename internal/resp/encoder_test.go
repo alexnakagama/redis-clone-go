9b12811d7e82d165cc13098dee7d27a239f00666
alexnakagama <alexnakagama4@gmail.com>
2026-08-20 19:49:39 -0300

    finished test for encoderror

--- internal/resp/encoder_test.go
+++ internal/resp/encoder_test.go
@@ -29,4 +29,19 @@ func TestEncodeSimpleString(t *testing.T) {
 	}
 }
 
-func TestEncodeError(t *testing.T) {}
+func TestEncodeError(t *testing.T) {
+	var buf bytes.Buffer
+
+	encoder := NewEncoder(&buf)
+
+	err := encoder.EncodeError("-ERR unknown command")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "-ERR unknown command\r\n"
+
+	if buf.String() != expected {
+		t.Errorf("expected %q, got: %q", expected, buf.String())
+	}
+}
