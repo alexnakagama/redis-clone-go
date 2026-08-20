d1703e035ba6bd61335d0b840051c3f1ab9e20ee
alexnakagama <alexnakagama4@gmail.com>
2026-08-20 20:17:49 -0300

    finished test for parse

--- internal/resp/parser_test.go
+++ internal/resp/parser_test.go
@@ -2,6 +2,29 @@ package resp
 
 import (
 	"testing"
+	"bufio"
+	"reflect"
+	"strings"
 )
 
-func TestParse(t *testing.T) {}
+func TestParse(t *testing.T) {
+	input := "*2\r\n" +
+		"$3\r\n" +
+		"GET\r\n" +
+		"$4\r\n" +
+		"name\r\n"
+
+	reader := bufio.NewReader(strings.NewReader(input))
+
+	result, err := Parse(reader)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []string{"GET", "name"}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
